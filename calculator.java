2d5951ee47ade86f87f529f84367526f332d0a2d
akshata bg <akshatabg69@gmail.com>
2024-02-08 11:36:30 +0530

    code Related to Basic Arith ops,square and cube values

--- calculator.java
+++ calculator.java
@@ -16,6 +16,17 @@ class calculator{
                 return ( operand_1/operand_2);
     }
 
+    float square(float operand_1){
+                return ( operand_1*operand_1);
+    }
+
+    
+    float cube(float operand_1){
+        return ( operand_1*operand_1*operand_1);
+    }
+
+    
+
     public static void main(String[] args){
         float operand_1=10;
         float operand_2=5;
@@ -31,6 +42,13 @@ class calculator{
 
         float div_result=calc.division(operand_1,operand_2);
         System.out.println("Result of division is:"+div_result);
+
+        float square_result=calc.square(operand_1);
+        System.out.println("Result of square is:"+square_result);
+
+        float cube_result=calc.cube(operand_1);
+        System.out.println("Result of cube is:"+cube_result);
+
         
     }
     
